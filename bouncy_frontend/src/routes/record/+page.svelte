3a4779334d2ef84af786ab4ec596a2d585b46f6c
Jakob Meier <mail@jakobmeier.ch>
2023-11-21 22:51:49 +0100

    basic camera

--- bouncy_frontend/src/routes/record/+page.svelte
+++ bouncy_frontend/src/routes/record/+page.svelte
@@ -1,2 +1,6 @@
-<p>[camera]</p>
+<script>
+  import Camera from './Camera.svelte';
+</script>
+
+<Camera />
 <p>[recording settings]</p>
---
+++ bouncy_frontend/src/routes/record/Camera.svelte
@@ -0,0 +1,105 @@
+<!-- User controls to start the camera and a video element that owns the camera feed. -->
+<script>
+  import { t } from '$lib/i18n';
+  import { onDestroy } from 'svelte';
+
+  export let size = '250px';
+  let cameraOn = false;
+  /** @type HTMLVideoElement */
+  let videoElement;
+
+  async function startCamera() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw new Error(
+        'Browser API navigator.mediaDevices.getUserMedia not available'
+      );
+    }
+
+    const videoConfig = {
+      audio: false,
+      video: {
+        facingMode: 'user',
+        width: 360,
+        height: 360,
+        frameRate: {
+          ideal: 60,
+        },
+      },
+    };
+
+    videoElement.srcObject =
+      await navigator.mediaDevices.getUserMedia(videoConfig);
+
+    await new Promise((resolve) => {
+      videoElement.onloadedmetadata = () => {
+        resolve(videoElement);
+      };
+    });
+    videoElement.play();
+
+    cameraOn = true;
+  }
+
+  function stopCamera() {
+    if (videoElement && videoElement.srcObject) {
+      videoElement.srcObject.getTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+    cameraOn = false;
+  }
+
+  onDestroy(stopCamera);
+</script>
+
+<div id="outer">
+  <div class="cam-container" style="min-height: {size}; min-width: {size};">
+    <!-- svelte-ignore a11y-media-has-caption -->
+    <video bind:this={videoElement} class:hidden={!cameraOn}></video>
+    {#if !cameraOn}
+      <span class="material-symbols-outlined"> videocam </span>
+    {/if}
+  </div>
+  <div>
+    {#if cameraOn}
+      <button on:click={stopCamera}>
+        <span class="material-symbols-outlined"> stop </span>
+        <p>{$t('record.stop-button')}</p>
+      </button>
+    {:else}
+      <button on:click={startCamera}>
+        <span class="material-symbols-outlined"> radio_button_unchecked </span>
+        <p>{$t('record.start-button')}</p>
+      </button>
+    {/if}
+  </div>
+</div>
+
+<style>
+  #outer {
+    margin: auto;
+    display: grid;
+    justify-items: center;
+  }
+
+  .cam-container {
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    margin: 10px auto;
+    border: 5px var(--theme-neutral-light) solid;
+    border-radius: 100px;
+    overflow: hidden;
+  }
+  .cam-container span {
+    font-size: 100px;
+  }
+
+  button {
+    width: 152px;
+    height: 95px;
+  }
+  button span {
+    font-size: 55px;
+  }
+</style>
