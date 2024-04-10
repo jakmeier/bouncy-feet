5e1952b313c8c682362b3f0ff5d5ceac1ac31bfb
Jakob Meier <mail@jakobmeier.ch>
2024-04-10 18:11:23 +0200

    allow copy-editing static dances

--- bouncy_frontend/src/routes/collection/dance/[slug]/edit/+page.svelte
+++ bouncy_frontend/src/routes/collection/dance/[slug]/edit/+page.svelte
@@ -2,6 +2,8 @@
   import { getContext } from 'svelte';
   import { page } from '$app/stores';
   import DanceEditor from '../../DanceEditor.svelte';
+  import { t } from '$lib/i18n';
+  import { DanceBuilder } from '$lib/instructor/bouncy_instructor';
 
   /** @type {import('./$types').PageData} */
   export let data;
@@ -10,14 +12,30 @@
   const localDances = localCollection.dances;
 
   const id = $page.params.slug;
-  /** @type {import('$lib/instructor/bouncy_instructor').DanceInfo} */
-  const dance = $localDances.find(
-    (
-      /** @type {import('$lib/instructor/bouncy_instructor').DanceInfo} */ dance
-    ) => dance.id === id
-  );
-  /** @type {import('$lib/instructor/bouncy_instructor').DanceBuilder} */
-  let danceBuilder = dance.asBuilder();
+
+  /** @type {DanceBuilder} */
+  let danceBuilder = createDanceBuilder(id);
+
+  /** @param {string} id */
+  function createDanceBuilder(id) {
+    // @ts-ignore
+    const localDance = $localDances.find((dance) => dance.id === id);
+
+    if (localDance) return localDance.asBuilder();
+
+    // not a local dance, try if it is an official dance
+    const officialDance = data.officialDances.find((dance) => dance.id === id);
+
+    if (officialDance) {
+      const builder = officialDance.asBuilder();
+      const copy = $t('editor.dance-copy-postfix');
+      builder.setId(`${id} (${copy})`);
+      return builder;
+    }
+
+    console.error('dance not found', id);
+    return new DanceBuilder(id);
+  }
 </script>
 
 <DanceEditor availableSteps={data.uniqueNameSteps} {danceBuilder} />
