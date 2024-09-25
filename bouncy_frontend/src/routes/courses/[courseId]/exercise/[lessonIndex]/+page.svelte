a56b203c717b119a181caa93201c1a141070be70
Jakob Meier <mail@jakobmeier.ch>
2024-09-25 16:23:10 +0200

    change text at lesson overview from 10x to 4x
    It really should show how many are defined in the course.
    But for now, with just one course, it doesn't really matter.

--- bouncy_frontend/src/routes/courses/[courseId]/exercise/[lessonIndex]/+page.svelte
+++ bouncy_frontend/src/routes/courses/[courseId]/exercise/[lessonIndex]/+page.svelte
@@ -60,7 +60,7 @@
   {#each lesson.parts as part, index}
     <div class="exercise-part">
       <Step step={part.step} poseIndex={$i} {animationTime} {size}></Step>
-      <b>10x</b>
+      <b>4x</b>
     </div>
     {#if index !== lesson.parts.length - 1}
       <div class="arrow">→</div>
