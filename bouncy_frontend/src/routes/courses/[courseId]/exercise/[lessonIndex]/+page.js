91d7af72289897931823e26978908bb834258536
Jakob Meier <mail@jakobmeier.ch>
2024-10-10 16:23:16 +0200

    ssr settings

---
+++ bouncy_frontend/src/routes/courses/[courseId]/exercise/[lessonIndex]/+page.js
@@ -0,0 +1,3 @@
+// with SSR enabled, directly accessing the page fails to load the video
+export const ssr = false;
+export const prerender = false;
---
+++ bouncy_frontend/src/routes/courses/[courseId]/exercise/[lessonIndex]/record/+page.js
@@ -0,0 +1,3 @@
+// with SSR enabled, directly accessing the page fails with a 404
+export const ssr = false;
+export const prerender = false;
