548e42b8d834abcd8f1058b2c086ec85f93224dd
Jakob Meier <mail@jakobmeier.ch>
2025-03-08 19:18:59 +0100

    yet another coach profile design

--- bouncy_frontend/src/routes/coach/select/+page.svelte
+++ bouncy_frontend/src/routes/coach/select/+page.svelte
@@ -4,7 +4,7 @@
   import AvatarStyleContext from '$lib/components/avatar/AvatarStyleContext.svelte';
   import AnimatedStep from '$lib/components/AnimatedStep.svelte';
   import StandardPage from '$lib/components/ui/StandardPage.svelte';
-  import { locale } from '$lib/i18n';
+  import { locale, t } from '$lib/i18n';
   import { base } from '$app/paths';
 
   const { getCourse } = getContext('courses');
@@ -12,51 +12,69 @@
   // TODO: read from user
   const selected = 'chorok';
   let coachWidth;
+
+  function selectCoach(coach) {
+    console.log('selected', coach);
+  }
 </script>
 
 <StandardPage white>
-  {#each coaches as coach}
-    <div
-      class="coach"
-      bind:clientWidth={coachWidth}
-      style="height: {1.2 * coachWidth}px"
-    >
-      <div class="avatar">
-        <AvatarStyleContext
-          coloring={coach.style.coloring}
-          bodyShape={coach.style.bodyShape}
-          headStyle={coach.style.headStyle}
-        >
-          <AnimatedStep
-            step={getCourse(coach.courseIds[0]).lessons[0].parts[0].step}
-            size={coachWidth}
-            backgroundColor={'var(--theme-neutral-light)'}
-          ></AnimatedStep>
-        </AvatarStyleContext>
+  <div class="coaches">
+    {#each coaches as coach}
+      <div
+        class="coach"
+        bind:clientWidth={coachWidth}
+        style="height: {1.25 * coachWidth}px"
+      >
+        <div class="avatar">
+          <AvatarStyleContext
+            coloring={coach.style.coloring}
+            bodyShape={coach.style.bodyShape}
+            headStyle={coach.style.headStyle}
+          >
+            <AnimatedStep
+              step={getCourse(coach.courseIds[0]).lessons[0].parts[0].step}
+              size={coachWidth}
+              backgroundColor={'var(--theme-neutral-light)'}
+            ></AnimatedStep>
+          </AvatarStyleContext>
+        </div>
+        <div class="title">{coach.title[$locale.substring(0, 2)]}</div>
         <div class="name" style="color: {coach.style.coloring.headColor}">
           {coach.name}
         </div>
-        <div class="title">{coach.title[$locale.substring(0, 2)]}</div>
         <div class="description">
           {coach.description[$locale.substring(0, 2)]}
         </div>
         {#if selected === coach.name}
-          <div class="selected">
-            <div>Current selection</div>
-            <img src="{base}/img/symbols/bf_eye.svg" alt="bf_eye" />
-          </div>
+          <!-- <div class="selected">
+      <div>Current selection</div>
+      <img src="{base}/img/symbols/bf_eye.svg" alt="bf_eye" />
+      </div> -->
         {/if}
+        <button onclick={() => selectCoach(coach)}>
+          {$t('coach.select-coach-button')}
+        </button>
       </div>
-    </div>
-  {/each}
+    {/each}
+  </div>
 </StandardPage>
 
 <style>
+  .coaches {
+    /* padding: 10rem 0; */
+  }
   .coach {
-    margin-top: 5rem;
+    display: grid;
+    margin-top: 8rem;
+    margin-bottom: 16rem;
     position: relative;
     width: min(90%, 60dvh);
     z-index: 1;
+  }
+
+  .coach:last-child {
+    margin-bottom: 4rem;
   }
 
   .coach div {
@@ -65,43 +83,29 @@
   }
 
   .name {
-    top: -4rem;
+    position: absolute;
+    top: -7rem;
     text-transform: capitalize;
     font-size: calc(2 * var(--font-large));
-    text-align: right;
-   */
-    mix-blend-mode: multiply;
+    /* text-align: right; */
+    /* mix-blend-mode: multiply; */
   }
 
   .title {
     rotate: -3deg;
-    top: 0.8rem;
+    top: -2.2rem;
     font-size: var(--font-normal);
   }
 
   .description {
-    top: 6rem;
+    top: 7rem;
     font-size: var(--font-nnormal);
-    padding: 2rem;
-    background-color: #FFFFFFFF;
+    width: 40vw;
   }
 
-  .coach .selected {
-    display: grid;
-    grid-template-columns: min-content auto;
-    gap: 1rem;
-    width: 100%;
-    top: -6rem;
-    align-items: center;
-    justify-content: end;
-    z-index: 1;
-  }
-  .coach .selected div {
-    position: relative;
-  }
-  .coach .selected img {
-    height: 3rem;
-    width: 3rem;
+  button {
+    align-self: flex-end;
+    justify-self: end;
   }
 
   .avatar {
@@ -109,5 +113,6 @@
     top: 0;
     height: min(80vw, 60dvh);
     width: min(80vw, 60dvh);
+    transform: translateX(20vw);
   }
 </style>
