4138aed208ec3015114cd3add9eabc073c212b82
Jakob Meier <mail@jakobmeier.ch>
2025-03-23 15:44:17 +0100

    try another safari codec fix
    I'm getting isTypeSupported() true but somehow recording doesn't actually work.
    Maybe reordering the codes makes it use the one that actually works?

--- bouncy_frontend/src/lib/media.js
+++ bouncy_frontend/src/lib/media.js
@@ -3,8 +3,8 @@ export function selectMediaMimeType() {
     // supported. Thus, let's try a few codecs and hope one works.
     // Note that "video/mp4" might fail isTypeSupported while more specific types works.
     const preferredTypes = [
+        'video/mp4;codecs="avc1.42E01E, mp4a.40.2"', // Strongly prefer this for Safari
         'video/mp4;codecs=avc1',
-        'video/mp4;codecs="avc1.42E01E, mp4a.40.2"',
         'video/webm;codecs=vp9',
         'video/webm;codecs=vp8',
         'video/webm',
