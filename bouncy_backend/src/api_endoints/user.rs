2753e129c4123cc96016f1a06c28e149313574ca
Jakob Meier <mail@jakobmeier.ch>
2025-09-08 14:38:13 +0200

    refactor(api): typed user response

--- bouncy_backend/src/api_endoints/user.rs
+++ bouncy_backend/src/api_endoints/user.rs
@@ -1,25 +1,30 @@
-//! Replaces sqlite based users.
-
 use crate::layers::oidc::AdditionalClaims;
 use crate::user::UserId;
-use axum::Extension;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::{Extension, Json};
 use axum_oidc::OidcClaims;
-use serde_json::json;
+
+#[derive(serde::Serialize)]
+pub struct UserInfoResponse {
+    id: i64,
+    sub: Option<String>,
+}
 
 pub async fn user_info(
     claims: Option<OidcClaims<AdditionalClaims>>,
     Extension(user_id): Extension<UserId>,
-) -> String {
-    if let Some(claims) = claims {
-        json!({
-            "id": user_id.num(),
-            "sub": claims.subject().as_str()
-        })
+) -> Response {
+    let user_info = if let Some(claims) = &claims {
+        UserInfoResponse {
+            id: user_id.num(),
+            sub: Some(claims.subject().to_string()),
+        }
     } else {
-        json!({
-            "id": user_id.num(),
-            "sub": null
-        })
-    }
-    .to_string()
+        UserInfoResponse {
+            id: user_id.num(),
+            sub: None,
+        }
+    };
+    (StatusCode::OK, Json(user_info)).into_response()
 }
