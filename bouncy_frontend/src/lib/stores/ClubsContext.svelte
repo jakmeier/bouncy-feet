477cb0b3e3e9e392e0f9c9a5b749bae8caeadbfc
Jakob Meier <inbox@jakobmeier.ch>
2026-01-31 23:04:47 +0100

    fix: loading my clubs in ClubsContext

--- bouncy_frontend/src/lib/stores/ClubsContext.svelte
+++ bouncy_frontend/src/lib/stores/ClubsContext.svelte
@@ -2,6 +2,7 @@
   import { onMount, setContext } from 'svelte';
   import { clubsData, loadMyClubs } from './Clubs.svelte';
   import { getUserContext } from '$lib/stores/context';
+  import { USER_AUH_STATE } from '$lib/enum_types';
 
   /**
    * @typedef {Object} Props
@@ -19,14 +20,16 @@
 
   const userCtx = getUserContext();
 
-  onMount(async () => {
-    // Note: It would be most sveltekit-like to load it on the server. But the
-    // svelte server doesn't have the user credentials from userCtx. Thus, club
-    // context is loaded empty first and dynamically filled in later. All pages
-    // using clubs must handle this.
-    // Blocking children to load doesn't work either, user interaction might be
-    // necessary to log in and get clubs loaded.
-    clubsData.mine = await loadMyClubs(userCtx);
+  // Note: It would be most sveltekit-like to load it on the server. But the
+  // svelte server doesn't have the user credentials from userCtx. Thus, club
+  // context is loaded empty first and dynamically filled in later. All pages
+  // using clubs must handle this.
+  // Blocking children to load doesn't work either, user interaction might be
+  // necessary to log in and get clubs loaded.
+  $effect(() => {
+    if (userCtx.authState !== USER_AUH_STATE.Anonymous) {
+      loadMyClubs(userCtx).then((clubs) => (clubsData.mine = clubs));
+    }
   });
 </script>
 
