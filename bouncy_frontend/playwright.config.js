cbd7208fa89db9ccbc72ca89de74f877731b3f9d
Jakob Meier <inbox@jakobmeier.ch>
2026-04-08 03:43:06 +0200

    test: update playwright settings

--- bouncy_frontend/playwright.config.js
+++ bouncy_frontend/playwright.config.js
@@ -1,14 +1,20 @@
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-    webServer: {
-        command: 'npm run dev',
-        port: 5173,
-        reuseExistingServer: true
-    },
     testDir: 'tests',
     testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-    baseURL: process.env.TEST_ENV || 'http://localhost:5173',
+    use: {
+        baseURL: process.env.TEST_ENV || 'http://localhost:5173',
+    }
 };
+
+// Only start webServer for local testing
+if (!process.env.TEST_ENV || process.env.TEST_ENV === 'http://localhost:5173') {
+    config.webServer = {
+        command: 'npm run dev',
+        port: 5173,
+        reuseExistingServer: true,
+    };
+}
 
 export default config;
