c51fcadb99c060dbfd626db6d3f4c5ec7ae5ece5
Jakob Meier <inbox@jakobmeier.ch>
2026-04-09 02:25:57 +0200

    PWA service worker

--- bouncy_frontend/vite.config.js
+++ bouncy_frontend/vite.config.js
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [
+        sveltekit(),
+        VitePWA({
+            strategies: 'generateSW',
+            workbox: {
+                // Precache app shell + hashed assets
+                globPatterns: ['**/*.{js,css,html,wasm}'],
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MiB
+            },
+        })
+    ],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     },
